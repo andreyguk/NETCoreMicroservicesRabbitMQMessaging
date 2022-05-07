033baf254b786c5fba4dc1b46ce8ff9ee15757a2
Andrii Huk <ahuk2@softserveinc.com>
2022-05-07 18:42:47 +0300

    add basic a Producer-Consumer project

---
+++ BasicTests/BasicRabbitMqProducerConsumer/BasicConsumer/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using RabbitMQ.Client;
+
+using RabbitMQ.Client.Events;
+using System.Text;
+
+GetMessage();
+
+void GetMessage()
+{
+    var factory = new ConnectionFactory() { HostName = "localhost" };
+
+    using (var connection = factory.CreateConnection())
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare("BasicQueueTest", false, false, false, arguments: null);
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (sender, args) =>
+        {
+            var body = args.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            Console.WriteLine("Received message {0}...", message);
+        };
+
+        channel.BasicConsume("BasicQueueTest", true, consumer);
+        Console.WriteLine("Press [enter] to exit the Consumer App");
+        Console.ReadLine();
+    }   
+}
+
---
+++ BasicTests/BasicRabbitMqProducerConsumer/BasicProducer/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+SendMessage();
+
+static void SendMessage()
+{
+
+    var connectionFactory = new ConnectionFactory() { HostName = "localhost" };
+
+    using (var connection = connectionFactory.CreateConnection())
+    {
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare("BasicQueueTest", false, false, false, null);
+
+            var message = "Message to RabbitMQ";
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish("", "BasicQueueTest", null, body);
+            Console.WriteLine("Send message {0}...", message);
+
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (ch, ea) =>
+            {
+                var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+
+                Console.WriteLine("Received message {0}...", content);
+                channel.BasicAck(ea.DeliveryTag, false);
+            };
+            channel.BasicConsume("BasicQueueTest", false, consumer);
+
+        }
+
+    }
+    Console.WriteLine("Press [enter] to exit the Sender App");
+    Console.ReadLine();
+}
