4b4f19898dbf8fb2d18e39af97267573c35f8540
Andrii Huk <ahuk2@softserveinc.com>
2022-05-10 15:23:44 +0300

    Initial commin for Infra Dependency project

---
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,40 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+
+            ////Subscriptions
+            //services.AddTransient<TransferEventHandler>();
+
+            ////Domain Events
+            //services.AddTransient<IEventHandler<TransferCreatedEvent>, TransferEventHandler>();
+
+            ////Domain Banking Commands
+            //services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TransferCommandHandler>();
+
+            ////Application Services
+            //services.AddTransient<IAccountService, AccountService>();
+            //services.AddTransient<ITransferService, TransferService>();
+
+            ////Data
+            //services.AddTransient<IAccountRepository, AccountRepository>();
+            //services.AddTransient<ITransferRepository, TransferRepository>();
+            //services.AddTransient<BankingDbContext>();
+            //services.AddTransient<TransferDbContext>();
+        }
+    }
+}
