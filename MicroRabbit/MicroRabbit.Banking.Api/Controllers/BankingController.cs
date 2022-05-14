d5eb719e5d87df269ac103c7b94fdb53c1e6299c
Andrii Huk <ahuk2@softserveinc.com>
2022-05-14 13:26:09 +0300

    implement publish and sendcommand for banking microservice

--- MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
+++ MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -1,4 +1,5 @@
 ﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
 using MicroRabbit.Banking.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,11 +23,11 @@ namespace MicroRabbit.Banking.Api.Controllers
             return Ok(_accountService.GetAccounts());
         }
 
-        //[HttpPost]
-        //public IActionResult Post([FromBody] AccountTransfer accountTransfer)
-        //{
-        //    _accountService.Transfer(accountTransfer);
-        //    return Ok(accountTransfer);
-        //}
+        [HttpPost]
+        public IActionResult Post([FromBody] AccountTransfer accountTransfer)
+        {
+            _accountService.Transfer(accountTransfer);
+            return Ok(accountTransfer);
+        }
     } 
 }
--- MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,7 +1,11 @@
-﻿using MicroRabbit.Banking.Application.Interfaces;
+﻿using MediatR;
+using MicroRabbit.Banking.Application.Interfaces;
 using MicroRabbit.Banking.Application.Services;
 using MicroRabbit.Banking.Data.Context;
 using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.CommandHandlers;
+using MicroRabbit.Banking.Domain.Commands;
+using MicroRabbit.Banking.Domain.Events;
 using MicroRabbit.Banking.Domain.Interfaces;
 
 using MicroRabbit.Domain.Core.Bus;
@@ -21,11 +25,12 @@ namespace MicroRabbit.Infra.IoC
             ////Subscriptions
             //services.AddTransient<TransferEventHandler>();
 
-            ////Domain Events
+            //Domain Events
             //services.AddTransient<IEventHandler<TransferCreatedEvent>, TransferEventHandler>();
 
-            ////Domain Banking Commands
-            //services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TransferCommandHandler>();
+
+            //Domain Banking Commands
+            services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TransferCommandHandler>();
 
             //Application Services
             services.AddTransient<IAccountService, AccountService>();
