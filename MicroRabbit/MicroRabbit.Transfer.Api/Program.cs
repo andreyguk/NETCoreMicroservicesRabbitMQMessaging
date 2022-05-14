7ed425ec9fd34abadda163df9b63accf33cf8d5c
Andrii Huk <ahuk2@softserveinc.com>
2022-05-14 14:04:24 +0300

    Partially completed Transfer microservices

--- MicroRabbit/MicroRabbit.Transfer.Api/Program.cs
+++ MicroRabbit/MicroRabbit.Transfer.Api/Program.cs
@@ -1,11 +1,24 @@
+using MediatR;
+using MicroRabbit.Infra.IoC;
+using MicroRabbit.Transfer.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Add services to the container.
+RegisterServices(builder.Services, builder.Configuration);
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Transfer Microservice", Version = "v1" });
+});
+builder.Services.AddMediatR(typeof(Program));
 
 var app = builder.Build();
 
@@ -23,3 +36,13 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void RegisterServices(IServiceCollection services, ConfigurationManager configuration)
+{
+    services.AddDbContext<TransferDbContext>(options =>
+    {
+        options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+    });
+
+    DependencyContainer.RegisterServices(services);
+}
